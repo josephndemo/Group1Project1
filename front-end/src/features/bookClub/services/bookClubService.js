9cc550165079bdfb95bc9b5802df87dedb9ae727
Joseph Ndemo <joseph.ndemo@student.moringaschool.com>
2026-07-07 17:42:04 -0400

    updated backend

--- front-end/src/features/bookClub/services/bookClubService.js
+++ front-end/src/features/bookClub/services/bookClubService.js
@@ -1,7 +1,7 @@
 import { mockBookClubBooks } from '../data/mockBookClubData.js';
 import { rankBooks } from '../utils/ranking.js';
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://127.0.0.1:5001';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://group1project1-1.onrender.com';
 const USE_MOCKS = import.meta.env.VITE_USE_MOCK_BOOK_CLUB !== 'false';
 const LOCAL_REVIEW_KEY = 'bookClub.mockReviews';
 
